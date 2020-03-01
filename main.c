a8a65d42e512e26b93c5440fcd8cd6b18d775666
philipchang411 <philipchang@mail.usf.edu>
2020-03-01 14:23:19 -0500

    Set up RDM
    First trial of RDM

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include <string.h>
 #include <stdbool.h>
 
-int * RDM(char file[255], int size, long* values);
-int * LRU(char file[255], int size, long* values);
-long * FIFO(char file[255], int size, long* values);
-int * VMS(char file[255], int size, long* values);
+long * RDM(char file[255], int size, long* values, bool debug);
+long * LRU(char file[255], int size, long* values);
+long * FIFO(char file[255], int size, long* values, bool debug);
+long * VMS(char file[255], int size, long* values);
 
 int main(int argc, const char* argv[])
 {
@@ -15,7 +15,7 @@ int main(int argc, const char* argv[])
     long diskWrites;
     long tempArray[3];
     long* values;
-
+    bool debug;
     //If there isn't any arguments
     if(argc == 1)
     {
@@ -38,16 +38,18 @@ int main(int argc, const char* argv[])
     char temp1[255];
     strcpy(temp1, argv[1]);
 
+    if(strcmp(argv[4], "debug")==0)
+        debug=true;
     //Determines which function to be directed to
     strcpy(temp, argv[3]);
     if(strcmp(temp, "fifo") == 0 || strcmp(temp, "FIFO") == 0)
-        values = FIFO(temp1, size, tempArray);
+        values = FIFO(temp1, size, tempArray, debug);
     else if(strcmp(temp, "LRU") == 0 || strcmp(temp, "lru") == 0)
-        LRU(temp1, size);
+        LRU(temp1, size, tempArray);
     else if(strcmp(temp, "RDM") == 0 || strcmp(temp, "rdm") == 0)
-        RDM(temp1, size);
+        values = RDM(temp1, size, tempArray, debug);
     else if(strcmp(temp, "VMS") == 0 || strcmp(temp, "vms") == 0)
-        VMS(temp1, size);
+        VMS(temp1, size, tempArray);
     else
     {
         printf("You did not input a valid function.\n");
@@ -63,68 +65,9 @@ int main(int argc, const char* argv[])
     return 0;
 }
 
-int * RDM(char file[255], int size, long* values)
+long * RDM(char file[255], int size, long* values, bool debug)
 {
-    FILE *inputFile = fopen(file, "r");
-    char temp[255];
-    
-    //Creates the Array based on the input
-    struct page
-    {
-        char name[24];
-        bool dirtyBit;
-    };
-    struct page pageTable[size];
-    while (fscanf(inputFile, "%s", temp) == 1) // expect 1 successful conversion
-    {
-        printf("Line: %s\n", temp);
-    }
-    if (feof(inputFile)) 
-    {
-        printf("All done\n");
-    }
-    else
-    {
-        printf("Ummmm Idk\n");
-    }
-
-    fclose(inputFile);
-    return values;
-}
-
-int * LRU(char file[255], int size, long* values)
-{
-    FILE *inputFile = fopen(file, "r");
-    char temp[255];
-    
-    //Creates the Array based on the input
-    struct page
-    {
-        char name[24];
-        bool dirtyBit;
-    };
-    struct page pageTable[size];
-    while (fscanf(inputFile, "%s", temp) == 1) // expect 1 successful conversion
-    {
-        printf("Line: %s\n", temp);
-    }
-    if (feof(inputFile)) 
-    {
-        printf("All done\n");
-    }
-    else
-    {
-        printf("Ummmm Idk\n");
-    }
-
-    fclose(inputFile);
-    return values;
-}
-
-//Needs to use a queue in order to keep track of whick to replace
-long * FIFO(char file[255], int size, long* values)
-{
-    FILE *inputFile = fopen(file, "r");
+   FILE *inputFile = fopen(file, "r");
     char temp[255];
     char address[255];
     char instruction[255];
@@ -148,9 +91,19 @@ long * FIFO(char file[255], int size, long* values)
         {
             temp[i] = address[i];
         }
-
+        
         fscanf(inputFile, "%s", instruction);
         bool present = false;
+        
+        if(debug == true)
+        {
+            printf("The current address: %s\nThe Current direction: %s\n", temp, instruction);
+
+            for(int i = 0; i < size; i++)
+            {
+                printf("Content in the %d element %s\n", i, pageTable[i].name);
+            }
+        }
         //If writing to the desk, check to the address is presentin the stack. If not then write to the disk. If it is, write to the frame and make dirty bit
         if(strcmp(instruction, "W"))
         {
@@ -168,23 +121,18 @@ long * FIFO(char file[255], int size, long* values)
                 //The frames aren't full so you don't need to take out
                 if(sizeOfTable != size)
                 {
-                    //INSERT for QUEUE Replaces the first input if it is full
-                    if(end == (size-1))
-                        end=-1;
-                    strcpy(pageTable[++end].name,temp);
+                    //INSERT
+                    strcpy(pageTable[sizeOfTable-1].name,temp);
                     sizeOfTable++;
                 }
                 else
                 {
                     //In order to move each data over, strcpy everything over one and insert in the beginnging
-                    for(int i = 0; i > size-1; i++)
-                    {
-                        strcpy(pageTable[i].name, pageTable[i+1].name);
-                        pageTable[i].dirtyBit = pageTable[i+1].dirtyBit;
-                    }
-                    strcpy(pageTable[size-1].name,temp);
-                    pageTable[size-1].dirtyBit = true;
-                    diskWrites++;
+                    int randomElement = rand() %size;
+                    struct page transfer = pageTable[randomElement];
+                    strcpy(pageTable[randomElement].name,temp);
+                    if(pageTable[randomElement].dirtyBit)
+                        diskWrites++;
                 }
                 
             }
@@ -239,7 +187,165 @@ long * FIFO(char file[255], int size, long* values)
     return values;
 }
 
-int * VMS(char file[255], int size, long* values)
+long * LRU(char file[255], int size, long* values)
+{
+    FILE *inputFile = fopen(file, "r");
+    char temp[255];
+    
+    //Creates the Array based on the input
+    struct page
+    {
+        char name[24];
+        bool dirtyBit;
+    };
+    struct page pageTable[size];
+    while (fscanf(inputFile, "%s", temp) == 1) // expect 1 successful conversion
+    {
+        printf("Line: %s\n", temp);
+    }
+    if (feof(inputFile)) 
+    {
+        printf("All done\n");
+    }
+    else
+    {
+        printf("Ummmm Idk\n");
+    }
+
+    fclose(inputFile);
+    return values;
+}
+
+//Needs to use a queue in order to keep track of whick to replace
+long * FIFO(char file[255], int size, long* values, bool debug)
+{
+    FILE *inputFile = fopen(file, "r");
+    char temp[255];
+    char address[255];
+    char instruction[255];
+    long diskReads =0;
+    long diskWrites =0;
+    long totalLines=0;
+    //Creates the Array based on the input
+    struct page
+    {
+        char name[24];
+        bool dirtyBit;
+    };
+
+    struct page pageTable[size];
+    int sizeOfTable = 0;
+    int end = -1;
+
+    while (fscanf(inputFile, "%s", address) == 1) // expect 1 successful conversion
+    {
+        for(int i = 0; i < strlen(address)-3; i++)
+        {
+            temp[i] = address[i];
+        }
+        
+        fscanf(inputFile, "%s", instruction);
+        bool present = false;
+        
+        if(debug == true)
+        {
+            printf("The current address: %s\nThe Current direction: %s\n", temp, instruction);
+
+            for(int i = 0; i < size; i++)
+            {
+                printf("Content in the %d element %s\n", i, pageTable[i].name);
+            }
+        }
+        //If writing to the desk, check to the address is presentin the stack. If not then write to the disk. If it is, write to the frame and make dirty bit
+        if(strcmp(instruction, "W"))
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if(strcmp(pageTable[i].name, temp) == 0)
+                {
+                    pageTable[i].dirtyBit = true;
+                    present = true;
+                }
+            }
+            //The data is not in the frames so you must insert
+            if(present == false)
+            {
+                //The frames aren't full so you don't need to take out
+                if(sizeOfTable != size)
+                {
+                    //INSERT for QUEUE Replaces the first input if it is full
+                    if(end == (size-1))
+                        end=-1;
+                    strcpy(pageTable[++end].name,temp);
+                    sizeOfTable++;
+                }
+                else
+                {
+                    //In order to move each data over, strcpy everything over one and insert in the beginnging
+                    for(int i = 0; i > size-1; i++)
+                    {
+                        strcpy(pageTable[i].name, pageTable[i+1].name);
+                        pageTable[i].dirtyBit = pageTable[i+1].dirtyBit;
+                    }
+                    if(pageTable[size-1].dirtyBit)
+                        diskWrites++;
+                    strcpy(pageTable[size-1].name,temp);
+                    pageTable[size-1].dirtyBit = true;
+                }
+                
+            }
+        }
+        //If the instruction is 'R'
+        else
+        {
+            for(int i = 0; i < size; i++)
+            {
+                if(strcmp(pageTable[i].name, temp) == 0)
+                    present = true;
+            }
+            //If the data isn't there, add it to the frame
+            if(present == false)
+            {
+                diskReads++;
+                if(sizeOfTable != size)
+                {
+                    //INSERT for QUEUE Replaces the first input if it is full
+                    if(end == (size-1))
+                        end=-1;
+                    strcpy(pageTable[++end].name,temp);
+                    sizeOfTable++;
+                }
+                else
+                {
+                    //In order to move each data over, strcpy everything over one and insert in the beginnging
+                    for(int i = 0; i > size-1; i++)
+                    {
+                        strcpy(pageTable[i].name, pageTable[i+1].name);
+                        pageTable[i].dirtyBit = pageTable[i+1].dirtyBit;
+                    }
+                    strcpy(pageTable[size-1].name,temp);
+                }
+            }
+        }
+        totalLines++;
+    }
+    if (feof(inputFile)) 
+    {
+        printf("All done\n");
+    }
+    else
+    {
+        printf("Ummmm Idk\n");
+    }
+
+    fclose(inputFile);
+    values[0] = diskReads;
+    values[1] = diskWrites;
+    values[2] = totalLines;
+    return values;
+}
+
+long * VMS(char file[255], int size, long* values)
 {
     FILE *inputFile = fopen(file, "r");
     char temp[255];
